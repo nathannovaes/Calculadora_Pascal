b1945f1b9f4716a799d24b679f24a6e4adae8c51
nvitiver <nathan.vitiver@gmail.com>
2017-05-25 17:25:45 -0300

    Version 0.2

---
+++ Trabalho Integrador (Lazarus)/trabalho.pas
@@ -0,0 +1,33 @@
+unit trabalho;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, FileUtil, Forms, Controls, Graphics, Dialogs, StdCtrls;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    Button1: TButton;
+    Button2: TButton;
+    Button3: TButton;
+    Button4: TButton;
+  private
+    { private declarations }
+  public
+    { public declarations }
+  end;
+
+var
+  Form1: TForm1;
+
+implementation
+
+{$R *.lfm}
+
+end.
+
---
+++ Trabalho Integrador (Lazarus)/trabalho_integrador.lpr
@@ -0,0 +1,21 @@
+program trabalho_integrador;
+
+{$mode objfpc}{$H+}
+
+uses
+  {$IFDEF UNIX}{$IFDEF UseCThreads}
+  cthreads,
+  {$ENDIF}{$ENDIF}
+  Interfaces, // this includes the LCL widgetset
+  Forms, trabalho
+  { you can add units after this };
+
+{$R *.res}
+
+begin
+  RequireDerivedFormResource:=True;
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
+
